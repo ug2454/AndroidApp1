d77720019525c3379075cf2ea2c487737a40919e
Uday Garg <u.garg14@gmail.com>
2020-12-20 20:21:31 +0530

    button interactivity and login page design

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    public void clickFunction(View view){
-        Log.i("Info","Button Clicked");
+    public void clickFunction(View view) {
+        EditText nameEditText = findViewById(R.id.nameEditText);
+        Log.i("Name","Button Clicked");
+
+        Toast.makeText(this, "Hello "+nameEditText.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
